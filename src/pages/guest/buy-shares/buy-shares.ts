125f32775ba68c612c66116591fca27832135943
akash-gupta-146 <ag.akash007@gmail.com>
2018-05-29 12:30:47 +0530

    updated theme

--- src/pages/guest/buy-shares/buy-shares.ts
+++ src/pages/guest/buy-shares/buy-shares.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
 
 @IonicPage()
 @Component({
   selector: 'page-buy-shares',
   templateUrl: 'buy-shares.html',
 })
-export class BuySharesPage {
+export class BuySharesPage implements OnInit {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
   }
 
   ionViewDidLoad() {
@@ -18,6 +18,29 @@ export class BuySharesPage {
 
   openContactPage(){
     this.navCtrl.push('ContactUsPage');
+  }
+
+  ngOnInit(){
+    window.addEventListener('scroll',()=>{
+      console.log('hello world');
+    });
+  }
+
+  setAnimations(){
+    var box:any = document.getElementsByClassName('delay-animation');
+    console.log('done');
+    for(var i=0; i<box.length;i++){
+      var boxPos = box[i].offsetTop;
+      var cursorPos = window.scrollY;
+
+      console.log(boxPos,cursorPos);
+      if(cursorPos >= boxPos - 800  ){
+        box[i].classList.add('slideUp');
+      }
+      if(cursorPos > boxPos + 800 ){
+        // box[i].classList.remove('animate');
+      }
+    }
   }
 
 }
