7f50be80f637a7f897544eb25f0f7d9a292627b0
Nitin Negi <nitinnegi0810@gmail.com>
2018-10-24 17:43:18 +0530

    fb and lineked login befire payment done, my account page created

---
+++ src/pages/guest/my-account/my-account.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AuthService } from '../../../providers/auth.service';
+import { CustomService } from '../../../providers/custom.service';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-my-account',
+  templateUrl: 'my-account.html',
+})
+export class MyAccountPage {
+
+  policies: Array<any>;
+  loadingPolicies = false;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private authService: AuthService,
+    private customService: CustomService
+    ) {  }
+
+  ionViewDidLoad() {
+    this.getPolicies();
+    }
+
+  getPolicies() {
+    this.loadingPolicies = true;
+    this.authService.fetchPolicies()
+      .subscribe((res: any) => {
+        this.policies = res;
+        this.loadingPolicies = false;
+      }, (err: any) => {
+        this.customService.showToast(err.msg);
+        this.loadingPolicies = false;
+      });
+  }
+
+  openPolicyDetail(policy: any) {
+    // this.navCtrl.push('PolicyDetailPage',{policy:policy});
+  }
+
+}
